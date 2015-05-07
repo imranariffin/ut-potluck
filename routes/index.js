1b33053b8c488927876e492d05ce69f9d058e71d
Anas Faris <anasfaris@yahoo.com>
2015-05-07 18:54:44 -0400

    Add bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'UT Summer Potluck' });
+  res.render('index', { 
+  	title: 'UT Summer Potluck' ,
+  	partials: {navbar: 'navbar'}
+  });
   //res.send(200);
   //{username: "imran", password: "pass"}
 });
--- routes/result.js
+++ routes/result.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/resultzzz', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+  	title: 'UT Summer Potluck' ,
+  	partials: {navbar: 'navbar'}
+  });
+  //res.send(200);
+  //{username: "imran", password: "pass"}
 });
 
 module.exports = router;
