0a816d71e8f670f77f96a12b4dfeb7bfe134542e
Imran Ariffin <imran.ariffin@mail.utoronto.ca>
2015-05-07 18:01:44 -0400

    edit resultzzz bug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UT Summer Potluck' });
-  //res.send(200);
+  res.send(200);
   //{username: "imran", password: "pass"}
 });
 
